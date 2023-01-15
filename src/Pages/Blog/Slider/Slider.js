4f905f088d5c6c38c80b83bff576ff807f135b9b
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-01-16 01:27:18 +0600

    added

--- src/Pages/Blog/Slider/Slider.js
+++ src/Pages/Blog/Slider/Slider.js
@@ -23,44 +23,6 @@ import { EffectCards } from "swiper";
 
 const Slider = () => {
 
-    const bannerData = [
-        {
-            image: img1,
-            prev: 2,
-            id: 1,
-            next: 2
-        },
-        {
-            image: img2,
-            prev: 1,
-            id: 2,
-            next: 3
-        },
-        {
-            image: img3,
-            prev: 3,
-            id: 3,
-            next: 1
-        },
-        // {
-        //     image: img4,
-        //     prev: 3,
-        //     id: 4,
-        //     next: 5
-        // },
-        // {
-        //     image: img5,
-        //     prev: 4,
-        //     id: 5,
-        //     next: 6
-        // },
-        // {
-        //     image: img6,
-        //     prev: 5,
-        //     id: 6,
-        //     next: 1
-        // }
-    ]
 
     return (
         <div className='' data-aos="zoom-out-right" data-aos-duration="2000">
