45d4331ac7656a447639122298f9232893db46b6
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-01-16 01:40:38 +0600

    added more

--- src/Pages/Blog/Slider/Slider.js
+++ src/Pages/Blog/Slider/Slider.js
@@ -5,7 +5,6 @@ import img3 from '../../../asstesFile/car6.webp'
 import img4 from '../../../asstesFile/car4.webp'
 import img5 from '../../../asstesFile/car5.webp'
 import img6 from '../../../asstesFile/car6.webp'
-import img7 from '../../../asstesFile/car6.webp'
 
 import './Slider.css'
 import SliderItem from './SliderItem';
@@ -20,13 +19,11 @@ import "swiper/css/effect-cards";
 
 // import required modules
 import { EffectCards } from "swiper";
+import { Typewriter } from 'react-simple-typewriter';
 
 const Slider = () => {
-
-
     return (
         <div className='' data-aos="zoom-out-right" data-aos-duration="2000">
-
             <div
                 style={{
                     backgroundColor: '#6b0f24',
@@ -51,8 +48,19 @@ const Slider = () => {
                     </div>
                     <div>
                         <div className='mr-4'>
-                            <h1 className="text-5xl font-bold">Choice Your Car</h1>
-                            <p className="py-6">Cars have controls for driving, parking, passenger comfort, and a variety of lights. Over the decades, additional features and controls have been added to vehicles, making them progressively more complex. These include rear-reversing cameras, air conditioning, navigation systems, and in-car entertainment. Most cars in use in the early 2020s are propelled by an internal combustion engine, fuelled by the combustion of fossil fuels. Electric cars, which were invented early in the history of the car, became commercially available in the 2000s and are predicted to cost less to buy than gasoline cars before 2025. The transition from fossil fuels to electric cars features prominently in most climate change mitigation scenarios, such as Project Drawdown's 100 actionable solutions for climate change.</p>
+                            <h1 className="text-5xl font-bold my-5">Choice Your Car</h1>
+                            <span className='text-white text-bold text-2xl mt-6'>
+                                {/* Style will be inherited from the parent element */}
+                                <Typewriter
+                                    words={['Cars have controls for driving, parking, passenger comfort, and a variety of lights. Over the decades, additional features and controls have been added to vehicles, making them progressively more complex. These include rear-reversing cameras, air conditioning, navigation systems, and in-car entertainment. Most cars in use in the early 2020s are propelled by an internal combustion engine, fuelled by the combustion of fossil fuels. Electric cars, which were invented early in the history of the car, became commercially available in the 2000s and are predicted to cost less to buy than gasoline cars before 2025. The transition from fossil fuels to electric cars features prominently in most climate change mitigation scenarios, such as Project Drawdown 100 actionable solutions for climate change.',]}
+                                    loop={5}
+                                    cursor
+                                    cursorStyle='_'
+                                    typeSpeed={60}
+                                    deleteSpeed={50}
+                                    delaySpeed={1000}
+                                />
+                            </span>
                         </div>
                         <div className="form-control">
                             <div className="">
